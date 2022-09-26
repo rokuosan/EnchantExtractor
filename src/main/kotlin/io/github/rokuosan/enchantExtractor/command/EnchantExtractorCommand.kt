c442a8fb7130c94f1106a80a83567c4a0f20b8ff
rokuosan <85651386+rokuosan@users.noreply.github.com>
2022-09-27 01:16:06 +0900

    タブ補完を実装

--- src/main/kotlin/io/github/rokuosan/enchantExtractor/command/EnchantExtractorCommand.kt
+++ src/main/kotlin/io/github/rokuosan/enchantExtractor/command/EnchantExtractorCommand.kt
@@ -1,33 +1,75 @@
 package io.github.rokuosan.enchantExtractor.command
 
+import io.github.rokuosan.enchantExtractor.Store
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.command.TabCompleter
 
 class EnchantExtractorCommand: CommandExecutor {
+    private lateinit var sender: CommandSender
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        this.sender = sender
         if(!command.name.equals("enchantExtractor", true)) return false
+        if(args.isEmpty()) sendHelpMessage().let { return true }
 
-        val first = if(args.isNotEmpty()){
-            args[0]
-        }else{
-            "help"
-        }
-
-        when(first){
-            "help" -> {
-
-            }
-            "reload" -> {
-
-            }
+        when(args[0]){
+            "help" -> sendHelpMessage()
+            "reload" -> reload()
+            "config.yml" -> showConfig()
+            else -> sender.sendMessage("Invalid argument")
         }
 
         return true
     }
 
-    fun sendHelpMessage(){
+    private fun showConfig(){
+        sender.sendMessage("config.yml:")
+        sender.sendMessage(Store.config.toString())
+    }
 
+    private fun sendHelpMessage(){
+        this.sender.sendMessage("""
+            Allowed Arguments:
+            - help
+            - reload
+            - config.yml
+        """.trimIndent())
+    }
+
+    private fun reload(){
+        val plugin = Store.plugin?:run{
+            this.sender.sendMessage("Fatal error: Please report this error code '1000' to administrator")
+            return
+        }
+        plugin.reloadConfig()
+        val list = mutableListOf<String>()
+        for(i in Store.configKeys){
+            plugin.config.getString(i)?.let { list.add(it) }
+        }
+        Store.config = list
+        this.sender.sendMessage("Successfully reloaded!")
+    }
+}
+
+class EnchantExtractorTabCompletion: TabCompleter {
+    override fun onTabComplete(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>
+    ): MutableList<String> {
+        return when(args.size){
+            1 -> {
+                mutableListOf(
+                    "help",
+                    "reload",
+                    "config.yml"
+                )
+            }
+
+            else -> mutableListOf("")
+        }
     }
 }
